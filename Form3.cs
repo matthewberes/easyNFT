d118d0c60ab4f390dd21a6a25733705035b7f07d
matthewberes <matthewberes@trentu.ca>
2022-10-10 21:12:22 -0400

    form3 view button draws images
    images are now drawn to form4 in the correct order,
    
    barebones implementation, more debugging and testing needed

--- Form3.cs
+++ Form3.cs
@@ -62,6 +62,20 @@ namespace easyNFT
 
         public void nftView(brs json)
         {
+            Dictionary<int, Bitmap> bmpDict = new Dictionary<int, Bitmap>();
+
+            Dictionary<int, string[]> pathDict = new Dictionary<int, string[]>();
+            pathDict[0] = json.filesAtr1;
+            pathDict[1] = json.filesAtr2;
+            pathDict[2] = json.filesAtr3;
+            pathDict[3] = json.filesAtr4;
+            pathDict[4] = json.filesAtr5;
+            pathDict[5] = json.filesAtr6;
+            pathDict[6] = json.filesAtr7;
+            pathDict[7] = json.filesAtr8;
+            pathDict[8] = json.filesAtr9;
+            pathDict[9] = json.filesAtr10;
+
             Control nftForView = this.Controls.Find("nftSelected", true)[0];
             //check for error, selected nft does not exist, if (nftSelected > curControls.curDict[nftArray].Length)
 
@@ -83,10 +97,18 @@ namespace easyNFT
                         if (json.nftArray[(int)((NumericUpDown)nftForView).Value].attributes[i] > (double)((NumericUpDown)curIndexLow).Value && json.nftArray[(int)((NumericUpDown)nftForView).Value].attributes[i] < (double)((NumericUpDown)curIndexHigh).Value)
                         {
                             MessageBox.Show("atr " + (i + 1).ToString() + " falls under index " + (j + 1).ToString(), json.nftArray[(int)((NumericUpDown)nftForView).Value].attributes[i].ToString());
+                            
+                            Bitmap tempPic = new Bitmap(pathDict[i][j]);
+                            bmpDict[i] = tempPic;
+                        
                         }
                     }                     
                 }
-                string test = json.filesAtr1[0]; 
+                if (i == json.atrNum)
+                {
+                    Form4 form4 = new Form4(bmpDict);
+                    form4.Show();
+                }
             } 
            
         }
---
+++ Form4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace easyNFT
+{
+    public partial class Form4 : Form
+    {
+        public Dictionary<int, Bitmap> bmpDict;
+
+        public Form4(Dictionary<int, Bitmap> sentDict)
+        {
+            bmpDict = sentDict;
+            InitializeComponent();
+            this.Refresh();
+        }
+
+        private void Form4_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graph = e.Graphics;
+            foreach (var curBmp in bmpDict)
+            {
+                Bitmap printCur = curBmp.Value;
+                graph.DrawImage(printCur, new Point(0, 0));
+            }
+        }
+    }
+}
